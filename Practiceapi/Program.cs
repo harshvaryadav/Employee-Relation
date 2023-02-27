d7a4627c82dc8372b429be5d859c34c80aa99411
Harshvardhan yadav <59766664+harshvaryadav@users.noreply.github.com>
2023-02-27 06:27:12 -0500

    Add files via upload

---
+++ Practiceapi/Program.cs
@@ -0,0 +1,29 @@
+using Practiceapi.Models;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+builder.Services.AddDbContext<ACE42023Context>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors(options=>options.WithOrigins("http://localhost:4200/").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());  
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
